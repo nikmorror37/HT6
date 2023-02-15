43e33b9996312860b0741e559c494bcbd4b15c63
nikmorror37 <nikitamironchuk37@gmail.com>
2023-02-15 18:01:08 +0300

    Initial commit

---
+++ HomeTask6ClassOOP/HomeTask6ClassOOP/Phone.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask6ClassOOP
+{
+    public class Phone
+    {
+        int number;
+        string model;
+        int weight;
+
+        string name;
+
+        public Phone(int initNumber, string initModel, int initWeight) 
+        {
+            number = initNumber; 
+            model = initModel; 
+            weight = initWeight;
+                
+        }   
+        
+        public void Characteristics()
+        {
+            Console.WriteLine($"Номер телефона {number}, модель телефона {model}, вес телефона {weight}");
+        }
+        
+        public void ReceiveCall(string initName)
+        {
+            name = initName; 
+            Console.WriteLine($"\nCalling {name}");
+        }
+        
+        public int GetNumber() 
+        {
+            return number;
+        }
+        
+        
+        
+        
+        
+        //public Phone()   //конструктор без параметров 
+        //{         
+            
+
+        //}
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ HomeTask6ClassOOP/HomeTask6ClassOOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace HomeTask6ClassOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //Phone phone = new Phone();
+            Phone iphone = new Phone(1023345,"iPhone", 150);
+            Phone samsung = new Phone(2034456,"Samsung", 160);
+            Phone xiaomi = new Phone(3045567,"Xiaomi", 140);
+
+            iphone.Characteristics(); //создал 3 экземпляра класса Phone
+            samsung.Characteristics(); 
+            xiaomi.Characteristics();
+
+            //Console.Write("\nEnter the name: "); 
+            //iphone.ReceiveCall(Console.ReadLine()); //вызов метода ReceiveCall
+
+            Console.WriteLine("\nНомер iphone  " + iphone.GetNumber());
+            Console.WriteLine("Номер samsung  " + samsung.GetNumber());
+            Console.WriteLine("Номер xiaomi  " + xiaomi.GetNumber());
+
+
+
+
+
+
+        }
+    }
+}
